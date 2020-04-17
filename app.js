5e61ec9c55e9c6f62ba9abf6462028d5e7af43eb
Souvik <souvikde.ns@gmail.com>
2020-04-18 00:32:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 
+app.get('/',(req,res)=>{
+    res.status(200).send('<h1>Ninja Developer Server</h1>')
+})
 
 app.listen(process.env.PORT|| 3000,()=>{
     console.log(`Server started at port ${process.env.PORT||3000}`)
