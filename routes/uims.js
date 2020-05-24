35f0065fdd49bbfc4539ce0de5b7d8b803014523
Souvik <souvikde.ns@gmail.com>
2020-05-24 14:22:31 +0530

    minor changes

--- routes/uims.js
+++ routes/uims.js
@@ -1,16 +1,36 @@
 const router = require('express').Router()
 const {getAttendance} = require('../lib/python-func')
 const {spawn} = require('child_process')
+const {PythonShell} = require('python-shell')
+const path = require('path')
 
 router.route('/attendence')
     .get(async(req,res)=> {
         const {uid, password} = req.body
-        let sub = spawn('python',['../lib/python/attendence.py',uid,password])
+        const options = {
+            args: [uid,password]
+        }
+        let pythonScript = new PythonShell(path.join(__dirname,'..','lib','python','attendence.py'),options)
 
-        sub.stdout.on('data', (data)=> {
-            let result = JSON.parse(data.toString())
+        let pythonKiller = setTimeout(() => {
+            pythonScript.childProcess.kill()
+        }, 27500)
+
+
+        pythonScript.on('message',(result) => {
             console.log(result)
-            res.status(200).json(result)
+
+            return res.status(200).json(JSON.parse(result.toString()))
+        })
+
+        pythonScript.end((err,signal, code) => {
+            console.log("Python execution was stopped!");
+            if (err){
+                console.log("Error from Python", err);
+                return res.status(404).send('Wrong data')
+            }
+
+            clearTimeout(pythonKiller)
         })
     })
 
