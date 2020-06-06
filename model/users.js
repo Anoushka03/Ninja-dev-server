45e4f43019b3fa58e63384aa743a5a248751a1f3
Souvik <souvikde.ns@gmail.com>
2020-06-06 15:20:22 +0530

    preparing user schema

--- model/users.js
+++ model/users.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
-const userShema = mongoose.Schema({
-    username: {
+const userSchema = new mongoose.Schema({
+    uid: {
         type: String
     },
     password: {
@@ -9,7 +10,32 @@ const userShema = mongoose.Schema({
     }
 })
 
+userSchema.pre('save', (next) => {
+    var user = this 
 
-const User = mongoose.model('user', userShema)
+    if(!user.isModified('password')) return next()
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if(err) return next(err)
+
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if(err) return next(err)
+
+            user.password = hash
+
+            next()
+        })
+    })
+})
+
+userSchema.methods.comparePassword = (candidatepassword, cb) => {
+    bcrypt.compare(candidatepassword, this.password, (err, isMatch) => {
+        if(err) return cb(err)
+
+        cb(null, isMatch)
+    })
+}
+
+const User = mongoose.model('user', userSchema)
 
 module.exports = User
