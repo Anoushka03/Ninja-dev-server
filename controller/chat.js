7fe254107c156bc1f0fcf4d27d8b90dd9a83e90c
Souvik <souvikde.ns@gmail.com>
2020-06-08 22:59:42 +0530

    Create chat.js

---
+++ controller/chat.js
@@ -0,0 +1,23 @@
+/**
+ * Create functions to respond to different features of chat
+ */
+
+// importing modules 
+const Chat = require('../model/chat')
+
+exports.createMessage = async (to, from, message) => {
+    
+    try {
+        const chat = Chat({
+            to: to,
+            from: from,
+            message: message
+        })
+        let res = await chat.save()
+
+        return res
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
